a1b93bd2b669b642779ab3b0465748b942e60f72
PARK-JONGSEOK <pjongsuk1@gmail.com>
2023-05-07 12:47:17 +0900

    Elementを探す方法

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 ## [Document について](https://github.com/PARK-JONGSEOK/BU4-13/pull/2)
 
 ## [JavaScript を利用して HTML の値を触る方法](https://github.com/PARK-JONGSEOK/BU4-13/pull/3)
+
+## [Element を探す方法](https://github.com/PARK-JONGSEOK/BU4-13/pull/４)
--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,11 @@
-const title = document.getElementById("title");
-console.log(title);
-title.innerHTML = "こんばんは🌙";
-console.log(title.className);
+const id = document.getElementById("byId");
+console.log(id);
+
+const classes = document.getElementsByClassName("byClass");
+console.log(classes);
+
+const h1 = document.getElementsByTagName("h1");
+console.log(h1);
+
+const querySelector = document.querySelector(".byQuery h1:last-child");
+console.log(querySelector);
