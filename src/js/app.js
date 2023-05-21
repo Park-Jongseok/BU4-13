c41813967227f9ff7b60ed768b0d4528a26d1a89
PARK-JONGSEOK <pjongsuk1@gmail.com>
2023-05-21 22:20:44 +0900

    Toggle!

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,7 @@
 const h1 = document.querySelector(".title h1");
 
 const handleClick = () => {
-    const clickedClassName = "clicked";
-    if (h1.classList.contains(clickedClassName)) {
-        h1.classList.remove(clickedClassName);
-    } else {
-        h1.classList.add(clickedClassName);
-    }
+    h1.classList.toggle("clicked");
 };
 
 h1.addEventListener("click", handleClick);
