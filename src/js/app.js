e0cc72fea60a6b19f799ec733107085175c722f6
PARK-JONGSEOK <pjongsuk1@gmail.com>
2023-05-21 22:14:20 +0900

    classNameを利用してCSS変更

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,12 @@
 const h1 = document.querySelector(".title h1");
 
 const handleClick = () => {
-    const currentColor = h1.style.color;
-    let newColor;
-    if (currentColor === "coral") {
-        newColor = "blue";
+    const clickedClassName = "clicked";
+    if (h1.className !== clickedClassName) {
+        h1.className = clickedClassName;
     } else {
-        newColor = "coral";
+        h1.className = "";
     }
-    h1.style.color = newColor;
-    console.log(currentColor);
 };
 
 h1.addEventListener("click", handleClick);
